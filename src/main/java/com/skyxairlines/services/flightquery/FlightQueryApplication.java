7166f61680d3e8a6b93c518b9586885e29c02d2c
Ricardo Corbera <rdcorbera@gmail.com>
2021-11-02 20:50:48 -0300

    Initial project

---
+++ src/main/java/com/skyxairlines/services/flightquery/FlightQueryApplication.java
@@ -0,0 +1,13 @@
+package com.skyxairlines.services.flightquery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightQueryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightQueryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skyxairlines/services/flightquery/FlightQueryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skyxairlines.services.flightquery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightQueryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
